62264e15026cfc09cd516d632275ce1dfd444ec4
John Doe <samikabdani.pro@gmail.com>
2024-12-16 16:46:27 +0100

    json lundi soir

---
+++ question.py
@@ -0,0 +1,24 @@
+import json
+
+class Question :
+    def __init__(self):
+        pass 
+
+    def level():
+        with open ("liste_questions.json", "r") as fichier :
+            data = json.load(fichier)
+
+        
+#si appel methode de dé = case niveau 1 and couleur = ... --> 
+
+
+'''
+
+3 niveaux de difficulté :
+- facile : 5 * nb de joueur
+- moyen : 6 * nb de joueur
+- difficile : nb de joueur 
+
+THEME
+
+'''
